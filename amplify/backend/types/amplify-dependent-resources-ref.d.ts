e026c88686be071c6ea25e91c016c4ab9a847254
Shushank Balaji Reddy <shushankinfsml@gmail.com>
2022-09-10 20:04:31 +0530

    Initial Comit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "hachiman2445224a": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
